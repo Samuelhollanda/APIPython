e7ac7c43ab49fe178c4b60f361ab08eddcd1263f
Samuelhollanda <samuelholandadev@outlook.com>
2026-05-18 11:12:13 -0300

    adcionando identacao ao script sql

--- criacao_db.sql
+++ criacao_db.sql
@@ -3,14 +3,14 @@ create database db_importdados;
 use db_importdados;
 
 CREATE TABLE TB_TITULOS(
-IDTitulo INT primary KEY auto_increment,
-Descricao varchar(120),
-DataTitulo date,
-DataVencimento date,
-IDCliente INT,
-NaturezaTitulo varchar(50),
-ValorTitulo float,
-SituacaoTitulo char(20)
+    IDTitulo INT primary KEY auto_increment,
+    Descricao varchar(120),
+    DataTitulo date,
+    DataVencimento date,
+    IDCliente INT,
+    NaturezaTitulo varchar(50),
+    ValorTitulo float,
+    SituacaoTitulo char(20)
 );
 
 
@@ -20,13 +20,15 @@ create table log_Titulo (
 	datalog date,
 	valor_old float,
 	valor_new float,
-	situacao char(25));
+	situacao char(25)
+);
 
 create or replace view VW_Resumo_Titulos as select situacaotitulo,
-count(*) as Qtde_titulos,
-sum(ValorTitulo) as Total_Valor,
-count(case when SituacaoTitulo='PENDENTE' then 1 else 0 end) as Pendente,
-count(case when SituacaoTitulo='QUITADO' then 1 else 0 end) as Quitado from TB_TITULOS group by situacaotitulo;
+    count(*) as Qtde_titulos,
+    sum(ValorTitulo) as Total_Valor,
+    count(case when SituacaoTitulo='PENDENTE' then 1 else 0 end) as Pendente,
+    count(case when SituacaoTitulo='QUITADO' then 1 else 0 end) as Quitado from TB_TITULOS group by situacaotitulo
+;
 
 DELIMITER $$
 
